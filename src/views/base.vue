e943c69c549479bd0b4d414211738ce9b65f7ead
hanbing426360 <435020142@qq.com>
2020-10-21 13:18:37 +0800

    基础

---
+++ src/views/base.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="base">
+		
+	</div>
+</template>
+
+<script>
+	export default {
+		name:"base",
+		data(){
+			return{
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	#base>>>{
+		
+	}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="home">
+		home
+		<el-button type="warning">home</el-button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:"home",
+		data(){
+			return{
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	#home>>>{
+		
+	}
+</style>
